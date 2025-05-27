44bda3395591334d8dce6b4d8df906da1dc8d9fc
Sandeep kumar <77495167+Sandeep-kumar-IITR@users.noreply.github.com>
2025-05-27 20:26:50 +0530

    Update settings.py

--- djangoback/settings.py
+++ djangoback/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7u%og39_i2qotd+dw3t%7(t+^)4p&za6=1v$h(!!&p-5s-f$50"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,14 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
     "myapp",
-    "corsheaders",
+    
 ]
 
 MIDDLEWARE = [
-     "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 
     "django.middleware.security.SecurityMiddleware",
@@ -57,6 +58,9 @@ MIDDLEWARE = [
 ]
 
 
+
+
+
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -65,15 +69,66 @@ REST_FRAMEWORK = {
     )
     
 }
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:9000",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://managemydoc.onrender.com",
+# ]
+
+# CORS_ALLOW_CREDENTIALS = True
+
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # CORS_ALLOW_ALL_ORIGINS = True  # Old name: CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
 
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-# https://sandeep-kumar-pets.onrender.com
-# ALLOWED_HOSTS = ['https://sandeep-kumar-pets.onrender.com']
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://managemydoc.onrender.com",  # Optional, if needed
+# ]
+# CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOW_CREDENTIALS = True
+
+
+# # https://sandeep-kumar-pets.onrender.com
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 ROOT_URLCONF = "djangoback.urls"
@@ -141,7 +196,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -149,12 +204,8 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# MEDIA_URL = '/static/'
-# MEDIA_ROOT = BASE_DIR / 
 
-
-
-
+MEDIA_ROOT = BASE_DIR / 'pdfs'
 
 
 
